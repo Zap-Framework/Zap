62557284e5a1c8fa6e016d05983e6a682da2f65b
Faris McKay <faris.mckay@hotmail.com>
2013-12-03 00:37:00 +0000

    Main File

---
+++ core/com/zap/Zap.java
@@ -0,0 +1,36 @@
+/**
+ *  This file is part of Zap Framework.
+ * 
+ *  Zap is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Zap is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Zap.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.zap;
+
+/**
+ *
+ * @author Faris
+ */
+public class Zap {
+	
+	public static final int PORT = 43594;
+	public static final int CYCLE_RATE = 600;
+    
+    public static void main(String[] args){
+        new Zap().start();
+    }
+    
+    public void start(){
+        
+    }
+
+}
